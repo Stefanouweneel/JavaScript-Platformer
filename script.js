bf31aeff4cbb22a7ed7614a7a5edbacf1bd92da6
Rachel Romee <RachelRomee@Rachels-MacBook-Pro.local>
2016-07-14 12:24:44 +0200

    count number of redraws

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
 var canvas = document.getElementById("board");
 var ctx = canvas.getContext("2d");
 var originalPosition = {};
-
 var playerPosition = { x: 100, y: 250 };
 var objectPosition = { x: 480, y: 250 };
-
 var floorPosition = { x: 0, y: 300 }
-
 var speed = 200;
-
-function renderPlayer(position, color) {
-  ctx.fillStyle = color;
-  ctx.fillRect(position.x, position.y, 20, 20);
-}
+var numberOfRedraws = 0;
 
 function renderFloor(position, color) {
   ctx.fillStyle = color;
   ctx.fillRect(position.x, position.y, 500, 200);
 }
 
+function renderPlayer(position, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(position.x, position.y, 20, 20);
+}
+
 function renderObject(position, color) {
   ctx.fillStyle = color;
   ctx.fillRect(position.x, position.y, 20, 2);
-  setTimeout(function() {
-    renderObject(objectPosition,"red");
-  }, 1000);
+}
+
+function renderConfuzer() {
+  ctx.fillStyle = color;
+  ctx.fillRect(position.x, position.y, 20, 2);
+
 }
 
 function movePlayer() {
@@ -69,6 +70,8 @@ function fallSpeed() {
 }
 
 function redraw() {
+  numberOfRedraws += 1;
+  console.log(numberOfRedraws)
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   renderFloor(floorPosition, "grey");
   renderPlayer(playerPosition, "green");
@@ -81,11 +84,13 @@ function redraw() {
   }, 30);
 }
 
+
+// renderConfuzer(objectPosition, "pink");
+
 function checkKey(e) {
   e = e || window.event;
   if (e.keyCode == '38') {
     e.preventDefault();
-    console.log("YAY");
     jumpPlayer();
   }
 }
